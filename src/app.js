91b7e823b37d6120f300fa59aba2e0fa0184f38a
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-08-06 20:39:13 +0100

    Amending stylesheet to SCSS and amending import in app.js to match

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GamesterApp from './components/GamesterApp';
-// import './styles/styles.scss';
+import './styles/styles.scss';
 
 ReactDOM.render(<GamesterApp />, document.getElementById('root'));
