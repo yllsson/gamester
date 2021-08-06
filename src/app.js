61cc816ed37d34ad1f42fb8ea79d2eabf09263db
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-08-06 20:40:20 +0100

    Add React.Strictmode in rendering

--- src/app.js
+++ src/app.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import GamesterApp from './components/GamesterApp';
 import './styles/styles.scss';
 
-ReactDOM.render(<GamesterApp />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <GamesterApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
