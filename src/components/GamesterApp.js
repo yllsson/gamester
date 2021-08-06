49168f49e80da0171d033cc08da81cd36452a806
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-08-06 23:01:32 +0100

    Creating fetch and amending fetch parameters

--- src/components/GamesterApp.js
+++ src/components/GamesterApp.js
@@ -1,47 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
 const GamesterApp = () => {
-  // const [gameData, setGameData] = useState();
+  const [gameData, setGameData] = useState([
+    {
+      name: 'Loading...'
+    }
+  ]);
 
-  // const myFetch = async () => {
-  //   const res = await fetch(
-  //     `https://api.rawg.io/api/games?key=14431a81669b494494dfb2bdf2b8e72a&search=${encodeURIComponent(
-  //       game
-  //     )}`
-  //   );
-  //   const data = await res.json();
-  //   console.log(data.results[0].metacritic);
+  const fetchGames = () => {
+    const games = fetch(
+      `https://api.rawg.io/api/games?key=14431a81669b494494dfb2bdf2b8e72a&page-size=3&platforms=1,2,3,4,7,18,186,187&ordering=-added&dates=2021-01-01,2021-10-01`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          console.log('Unable to fetch data');
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setGameData(data.results);
+        return data;
+      });
 
-  //   info.innerText = data.results[0].name;
-  //   ratingInfo.innerText = data.results[0].metacritic;
-  // };
+    return games;
+  };
 
-  //   const searchForm = document.getElementById('searchForm');
-  // const info = document.getElementById('info');
-  // const ratingInfo = document.getElementById('rating');
-
-  // useEffect((arguments) => {
-  //   setGameData(myFetch())
-  // }, [])
+  useEffect(() => {
+    fetchGames();
+  }, []);
 
   return (
     <main>
       <h1>Gamester</h1>
 
-      <form
-        // onSubmit={(event) => {
-        //   event.preventDefault();
-        //   const searchInput = document.getElementById('searchInput');
-        //   myFetch(searchInput.value);
-        // }}
-        id='searchForm'
-      >
-        <input id='searchInput' type='text' placeholder='Game' />
-        <button>Submit</button>
-      </form>
-      <h2 id='info'>Name of the game</h2>
-      <h2>Metascore:</h2>
-      <p id='rating'>Rating</p>
+      {gameData.map((game, index) => {
+        return <p key={index}>{game.name}</p>;
+      })}
     </main>
   );
 };
