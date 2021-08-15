6393cbf71a1a1465f412ba8f04e7c0aa43189bb6
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-08-15 17:20:39 +0100

    Attempting to solve fetch issues

--- client/src/components/GamesterApp.js
+++ client/src/components/GamesterApp.js
@@ -2,16 +2,10 @@ import React, { useState, useEffect } from 'react';
 import GameCard from '../components/GameCard';
 
 const GamesterApp = () => {
-  const [gameData, setGameData] = useState([
-    {
-      name: 'Loading...'
-    }
-  ]);
+  const [gameData, setGameData] = useState(null);
 
   const fetchGames = () => {
-    const games = fetch(
-      `https://api.rawg.io/api/games?key=14431a81669b494494dfb2bdf2b8e72a&page-size=3&platforms=1,2,3,4,7,18,186,187&ordering=-added&dates=2021-01-01,2021-10-01`
-    )
+    fetch(`/games`)
       .then((res) => {
         if (!res.ok) {
           console.log('Unable to fetch data');
@@ -20,33 +14,34 @@ const GamesterApp = () => {
         }
       })
       .then((data) => {
-        console.log(data.results);
-        setGameData(data.results);
-        return data;
+        console.log(data);
+        setGameData(data);
       });
-
-    return games;
   };
 
   useEffect(() => {
     fetchGames();
+    console.log(gameData);
   }, []);
 
   return (
     <main>
       <h1>Gamester</h1>
-
-      {gameData.map((game, index) => {
-        return (
-          <GameCard
-            name={game.name}
-            image={game.background_image}
-            rating={game.rating}
-            metacritic={game.metacritic}
-            key={index}
-          />
-        );
-      })}
+      {!gameData ? (
+        <h2>loading...</h2>
+      ) : (
+        gameData.map((game, index) => {
+          return (
+            <GameCard
+              name={game.name}
+              image={game.background_image}
+              rating={game.rating}
+              metacritic={game.metacritic}
+              key={index}
+            />
+          );
+        })
+      )}
     </main>
   );
 };
