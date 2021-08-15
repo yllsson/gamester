34b92fe80fcb1f22f1b71b60eb9cfec57cdf6b63
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-08-15 13:23:26 +0100

    Solved the request issue in my server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,10 @@
 const fetchGames = require('./utils/fetchGames');
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 3000;
 
 app.get('/games', (req, res) => {
-  // 14/08 - Trying to figure out why my fetch isn't working... It's clearly logging correctly in the console but it does not show up on port/games! Gotta continue on next time.
-  const test = fetchGames();
-  res.send({
-    results: test
-  });
+  fetchGames(res);
 });
 
 app.listen(port, () => {
